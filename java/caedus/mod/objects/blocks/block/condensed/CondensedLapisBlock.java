b5e70c2aee8f40b21f46cc688c435b9aa2c7b5f1
Caedus-Renegade <black_angel_zer0@hotmail.com>
2018-06-19 00:50:38 -0500

    Add files via upload

---
+++ java/caedus/mod/objects/blocks/block/condensed/CondensedLapisBlock.java
@@ -0,0 +1,35 @@
+package caedus.mod.objects.blocks.block.condensed;
+
+import caedus.mod.Main;
+import caedus.mod.init.BlockInit;
+import caedus.mod.init.ItemInit;
+import caedus.mod.util.interfaces.IHasModel;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+
+public class CondensedLapisBlock extends Block implements IHasModel 
+{
+	public CondensedLapisBlock(String name)
+	{
+		super(Material.IRON);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setHardness(6);
+		setResistance(2);
+		setHarvestLevel("pickaxe", 1);
+		setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+		
+		BlockInit.BLOCKS.add(this);
+		ItemInit.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+	}
+	
+	@Override
+	public void registerModels()
+	{
+		Main.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+	}
+}
---
+++ java/caedus/mod/objects/blocks/block/condensed/CondensedPorkchopCrate.java
@@ -0,0 +1,35 @@
+package caedus.mod.objects.blocks.block.condensed;
+
+import caedus.mod.Main;
+import caedus.mod.init.BlockInit;
+import caedus.mod.init.ItemInit;
+import caedus.mod.util.interfaces.IHasModel;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+
+public class CondensedPorkchopCrate extends Block implements IHasModel 
+{
+	public CondensedPorkchopCrate(String name)
+	{
+		super(Material.WOOD);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setHardness(2);
+		setResistance(2);
+		setSoundType(SoundType.WOOD);
+		setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+		
+		BlockInit.BLOCKS.add(this);
+		ItemInit.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+	}
+	
+	@Override
+	public void registerModels()
+	{
+		Main.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+	}
+}
