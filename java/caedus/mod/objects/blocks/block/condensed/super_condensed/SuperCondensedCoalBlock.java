b3f8d04e67f440f1bd3a5466607bee25dd8e5f79
Caedus-Renegade <black_angel_zer0@hotmail.com>
2018-06-19 00:49:56 -0500

    Add files via upload

---
+++ java/caedus/mod/objects/blocks/block/condensed/super_condensed/SuperCondensedCoalBlock.java
@@ -0,0 +1,40 @@
+package caedus.mod.objects.blocks.block.condensed.super_condensed;
+
+import caedus.mod.Main;
+import caedus.mod.init.BlockInit;
+import caedus.mod.init.ItemInit;
+import caedus.mod.util.interfaces.IHasModel;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+
+public class SuperCondensedCoalBlock extends Block implements IHasModel 
+{
+	public SuperCondensedCoalBlock(String name)
+	{
+		super(Material.ROCK);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setHardness(9);
+		setResistance(2);
+		setHarvestLevel("pickaxe", 0);
+		setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+		
+		BlockInit.BLOCKS.add(this);
+		ItemInit.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+	}
+	
+	public int getItemBurnTime(ItemStack itemStack)
+	{
+		return 1166400;
+	}
+	
+	@Override
+	public void registerModels()
+	{
+		Main.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+	}
+}
